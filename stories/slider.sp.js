7b4318865060d934a0387eff3fe70e39a57ec2be
Thorsten Scherler <scherler@gmail.com>
2017-08-25 15:27:03 +0200

    [speed] better code

--- stories/slider.sp.js
+++ stories/slider.sp.js
@@ -5,20 +5,19 @@ import Slide from '../src/main/js/slider/Slide';
 import Slider from '../src/main/js/slider/Slider';
 import { beforeEach, storiesOf, describe, it, specs } from '../.storybook/facade';
 
+const slide = {
+    title: "Pimp my Blue Ocean",
+    description: "### create a custom plugin \n" +
+    "\n" +
+    "We will use [JenkinsWorld 2017 BO seed](https://github.com/scherler/jw17boseed)\n" +
+    "\n" +
+    "### customize Blue Ocean\n" +
+    " - with custom css\n" +
+    " - custom components",
+};
 const stories = storiesOf('Slider', module);
-
 let storyName = 'Slide';
 stories.add(storyName, () => {
-    const slide = {
-        title: "Pimp my Blue Ocean",
-        description: "### create a custom plugin \n" +
-        "\n" +
-        "We will use [JenkinsWorld 2017 BO seed](https://github.com/scherler/jw17boseed)\n" +
-        "\n" +
-        "### customize Blue Ocean\n" +
-        " - with custom css\n" +
-        " - custom components",
-    };
     const story = (<div className="story">
         <Slide slide={slide}/>
     </div>);
@@ -32,30 +31,19 @@ stories.add(storyName, () => {
     return story;
 });
 
+const slides = [
+    slide,
+    {
+        title: "Deliver Blue Ocean Components at the Speed of Light",
+        description: "Using storybook.js.org for Blue Ocean frontend to \n" +
+        "\n" +
+        "- speed up the development \n" +
+        "- speed up the delivery process \n" +
+        "- validate with PM and designer the UX. ",
+    }
+];
 storyName = 'Slider';
 stories.add(storyName, () => {
-    const slides = [
-        {
-            title: "Pimp my Blue Ocean",
-            description: "### create a custom plugin \n" +
-            "\n" +
-            "We will use [JenkinsWorld 2017 BO seed](https://github.com/scherler/jw17boseed)\n" +
-            "\n" +
-            "### customize Blue Ocean\n" +
-            " - with custom css\n" +
-            " - custom components",
-        },
-        {
-            title: "Deliver Blue Ocean Components at the Speed of Light",
-            description: "## Deliver Blue Ocean Components at the Speed of Light\n" +
-            "\n" +
-            "Using storybook.js.org for Blue Ocean frontend to \n" +
-            "\n" +
-            "- speed up the development \n" +
-            "- speed up the delivery process \n" +
-            "- validate with PM and designer the UX. ",
-        }
-    ];
     const story = (<div className="story">
         <Slider slides={slides}/>
     </div>);
