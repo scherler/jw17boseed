6c278e2f3f5b304a9a6a4c2d17798327f9e17de9
Thorsten Scherler <scherler@gmail.com>
2018-09-17 22:51:49 +0200

    [updtae] update versions and add workaround for dep issues

--- src/main/js/Logo.jsx
+++ src/main/js/Logo.jsx
@@ -5,7 +5,7 @@ export default class Logo extends Component {
     render() {
         // return the component
         return (<a className="MasterLogo BlueOceanLogo MyLogo">
-            <Icon icon="bookmark" size={50} />
+            <Icon icon="ActionBookmark" size={50} />
             My Jenkins</a>);
     }
 }
