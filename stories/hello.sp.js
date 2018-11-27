0a79523ec68f3c0e75e2f1013eba1b3acfe19b31
Thorsten Scherler <scherler@gmail.com>
2018-11-28 00:03:48 +0100

    chore: [master] fix storybook header

--- stories/hello.sp.js
+++ stories/hello.sp.js
@@ -9,14 +9,15 @@ const stories = storiesOf('Hello World', module);
 
 let storyName = 'say hello to unit testing in storybook';
 stories.add(storyName, () => {
-    const story = (<div className="hello">
-        <Logo />
-        <div className="teaser">
+    const story = (<div>
+        <div className="BasicHeader BasicHeader--default ContentPageHeader">
+            <Logo />
+        </div>
+        <div className="hello">
             Hello
         </div>
     </div>);
     const wrapper = mount(story);
-
     specs(() => describe(storyName, () => {
         it('renders div saying hello', () => {
             expect(wrapper.find('div.hello').length).toBe(1)
